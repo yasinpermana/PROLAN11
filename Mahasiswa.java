719623de321403ddbaecf19452147f1d5561b51a
yasinpermana <yasinpermana297@gmail.com>
2017-02-28 15:49:20 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,10 @@
+public class Mahasiswa
+{
+public static void main(String []args)
+	{
+System.out.println("Nama   : Muhamad Yasin Permana");
+System.out.println("Nim    : 10113412");
+System.out.println("Kelas  : Prolan-11");
+System.out.println("Alamat : Sekeloa timur no 1/152 b");
+	}
+}
