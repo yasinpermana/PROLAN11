6b172c5cd4021d6b46468e1ebd0c93eb8a66df37
yasinpermana <yasinpermana297@gmail.com>
2017-03-07 14:48:02 +0700

    Add files via upload

---
+++ Pertemuan 3/Mahasiswa.java
@@ -0,0 +1,17 @@
+public class Mahasiswa {
+	public String nama;
+	
+	public void Kelas () {
+			System.out.println ("Muhamad Pasin Permana");
+			System.out.println ("Kelas Prolan 11");
+	}		
+	
+	public static void main(String []args){
+		
+			Mahasiswa Yasin = new Mahasiswa();
+			
+			Yasin.nama = "Muhamad Yasin Permana";
+			Yasin.Kelas();
+		
+	}
+}
