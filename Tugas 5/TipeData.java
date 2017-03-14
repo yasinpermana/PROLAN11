b06a3ba7154b155d9647f730949e1594dece5d3e
yasinpermana <yasinpermana297@gmail.com>
2017-03-14 22:20:03 +0700

    Add files via upload

---
+++ Tugas 5/TipeData.java
@@ -0,0 +1,58 @@
+public class TipeData{
+  public static void main(String[] args){
+   // tipe data objek
+   String nama="Gundam";
+   System.out.println(nama.length());
+   
+
+      //tipe data short
+   short maxShort=32767;
+   short minShort=-32767;
+   short defaultShort=0;
+   System.out.println(defaultShort);
+   
+   //tipe data long
+   long maxLong=9223372036854775807L;
+   long minLong=-922337203685477580L;
+   long defaultLong=0L;
+   System.out.println(defaultLong);
+   
+   //tipe data float
+   float maxFloat=3.4028234799999999999999999999999999f;
+   float minFloat=-3.40282347999999999999999999999999f;
+   float defaultFloat=0.0f;
+   System.out.println(maxFloat);
+   System.out.println(minFloat);
+   
+   //tipe data byte
+   byte makByte=127;
+   byte minByte=-127;
+   byte defaultByte=0;
+   System.out.println(defaultByte);
+   
+   //tipe data int
+   int maxInt =214783648;
+   int minInt =-214783648;
+   int defaultInt=0;
+   System.out.println(defaultInt);
+   
+   //tipe data long
+   long maxLong=9223372036854775807L;
+   long minLong=-922337203685477580L;
+   long defaultLong=0L;
+   System.out.println(defaultLong);
+   
+   //tipe data float
+   float maxFloat=3.4028234799999999999999999999999999f;
+   float minFloat=-3.40282347999999999999999999999999f;
+   float defaultFloat=0.0f;
+   System.out.println(maxFloat);
+   System.out.println(minFloat);
+   
+   //tipe data double
+   double randomDouble=1.797693134862315702d;
+   double randomDouble1=-1.797693134862315702d;
+   double defaultDouble=0.0;
+   System.out.println(randomDouble);
+   }
+ }
