65347d87e0b71cdaed305f933dd4dcfd8d2a495a
yasinpermana <yasinpermana297@gmail.com>
2017-03-21 21:23:15 +0700

    lupa satu file ketinggalan

---
+++ Tugas 6/TypeGundam.java
@@ -0,0 +1,13 @@
+
+public class TypeGundam {
+    public static void main(String[]args){
+     
+      Tampilan TypeGundam ;
+        TypeGundam = new Tampilan();
+      
+      TypeGundam.Layar();
+
+    
+    }
+    
+}
