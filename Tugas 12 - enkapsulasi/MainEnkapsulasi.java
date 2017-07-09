cbbf94cd46802b65d2ca5a110ae0d36c0db851b8
yasinpermana <yasinpermana297@gmail.com>
2017-07-09 17:16:21 +0700

    Add files via upload

---
+++ Tugas 12 - enkapsulasi/MainEnkapsulasi.java
@@ -0,0 +1,11 @@
+public class MainEnkapsulasi{
+	
+	public static void main(String[] args){
+	
+		enkapsulasi enka = new enkapsulasi();
+		enka.setOpsi1(10115104);
+		System.out.println(enka.getOpsi1());
+		enka.setOpsi2("Muhamad Yasin Permana");
+		System.out.println(enka.getOpsi2());
+	}
+}
---
+++ Tugas 12 - enkapsulasi/enkapsulasi.java
@@ -0,0 +1,21 @@
+public class enkapsulasi{
+	
+	private int opsi1;
+	private String opsi2;
+	
+	public int getOpsi1(){
+		return opsi1;
+	}
+
+	public void setOpsi1(int opsi1){
+		this.opsi1 = opsi1;
+	}
+
+	public String getOpsi2(){
+		return opsi2;
+	}
+	
+	public void setOpsi2(String opsi2){
+		this.opsi2 = opsi2;
+	}
+}
